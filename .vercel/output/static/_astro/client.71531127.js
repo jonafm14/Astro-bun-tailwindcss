d69d630101bca0e8a306fb05644993cea616e1de
jona <jonamusciachio@gmail.com>
2023-09-18 14:46:49 -0300

    Addd vercel

---
+++ .vercel/output/static/_astro/client.71531127.js
@@ -0,0 +1,1 @@
+import{c as f}from"./client.8ef2a4eb.js";export{f as default};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
