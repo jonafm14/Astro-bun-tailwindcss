42b1389176495cfa7c51fc2fd87c365019e80516
jona <jonamusciachio@gmail.com>
2023-09-18 15:18:53 -0300

    add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from '@astrojs/preact';
-import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
   }),
 });
