4f855671a6e4ef16ad1629df2b187cee47eeb750
jona <jonamusciachio@gmail.com>
2023-09-18 15:28:41 -0300

    Change method to render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from '@astrojs/preact';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
